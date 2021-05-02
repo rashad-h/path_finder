d882fd87bbc31b7f06c92d3e94c16bf0f526f01d
rashad-h <sc20rh@leeds.ac.uk>
2021-05-02 17:49:58 +0430

    Finished writing graphics tests

--- cw2/graphics.c
+++ cw2/graphics.c
@@ -2,4 +2,6 @@
 
 // functions used to draw the resutls
 // return 0 if correct
-int show_results(struct NodeArray shortest_path);
+int show_results(struct NodeArray shortest_path) {
+    return 1;
+}
--- cw2/unit_tests_graphics.c
+++ cw2/unit_tests_graphics.c
@@ -2,9 +2,26 @@
 #include "graphics.h"
 
 
-void test_()
+void test_show_results()
 {
+    struct NodeArray test_nodes;
+    test_nodes.array = (struct Node* ) malloc(3 * sizeof(struct Node));
+    test_nodes.length = 3;
+    test_nodes.array->id = 0;
+    test_nodes.array->lat = 3.1;
+    test_nodes.array->lon = 1.2;
 
+    (test_nodes.array+1)->id = 1;
+    (test_nodes.array+1)->lat = 1.5;
+    (test_nodes.array+1)->lon = 1.9;
+
+    (test_nodes.array+2)->id = 2;
+    (test_nodes.array+2)->lat = 2.3;
+    (test_nodes.array+2)->lon = 2.8;
+    
+    int ans = show_results(test_nodes);
+    TEST_ASSERT_EQUAL_INT (0, ans);
+    
 }
 
 void setUp()
@@ -19,7 +36,7 @@ void tearDown()
 int main()
 {
     UNITY_BEGIN();
-    RUN_TEST();
+    RUN_TEST(test_show_results);
 
     return UNITY_END();
 }
