15e2693633ac4bb35ee6ffed6055b0491ba34034
rashad-h <rashad.hosseini@icloud.com>
2024-03-07 00:34:51 +0000

    Updated structure of files

--- README.txt
+++ README.txt
@@ -1,7 +1,20 @@
-Librarian account:
-username: librarian
-password: librarian
+Application name: route_finding
 
-Some books and accounts are already added. check users.txt and books.txt
+git repository: https://gitlab.com/comp1921_2021/sc20rh.git
 
-All functionalities test and works perfectly as intended on linux. 
+This program will read any .map file with the same format of the original file. 
+And it will give you the path between any two nodes by Dijkstra of Floyd's algorithm and you can compare the results. 
+Also the program will paint the graph using GNUPLOT when you exit the program. 
+
+You can choose the nodes by their id or coordinates. 
+
+Some random nodes chosen for testing:
+
+id		lat		lon
+-2560		53.807613	-1.554711
+1615401915	53.804646	-1.554947
+1968799687	53.804071	-1.555334
+247958669	53.805909	-1.556134
+1615404345	53.804747	-1.555335
+-2486		53.809637	-1.557703
+49510370		53.802597	-1.558777
--- main.c
+++ main.c
@@ -1,128 +1,230 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "register_login.h"
-//#include "book_management.h"
-
-struct UserArray all_users; //array of all users
-struct BookArray all_books; //array of all books
-
+#include "input.h"
+#include "logic.h"
+#include "graphics.h"
 
 int main(int argc, char const *argv[])
 {
+
+    // reading the .map file
+    FILE* map;
+    if (!read_map(map))
+    {
+        printf("\nMap was loaded successfully!\n\n");
+    }
     
-    loan_length = 0;
-    all_users.length = 0;
-    all_users.array = (struct User* ) malloc(sizeof(struct User));
 
-
-    
-    //variable used for loading
-    struct Book loaded_book;
-
-    char* loaded_name;
-    char* loaded_username;
-    char* loaded_password;
-    char* loaded_email;
-
-    unsigned int ID_loaded; 
-    char* title_loaded; 
-    char* authors_loaded; 
-    char* user_id_loaded;
-
-    //loading
-    FILE* file1;
-    load_books(file1);
-    load_users(file1);
-    load_loans(file1);
-
-    
     while (1)
     {
-        printf("\n");
-        printf("Please choose an option:\n");
-        printf("1) Register an account\n");
-        printf("2) Login\n");
-        printf("3) Search for books\n");
-        printf("4) Display all books\n");
-        printf("5) Quit\n");
-        printf(" Option: ");
-
-
-        
-        
-        
-
+        printf("To choose the points by ID enter 1, to choose by coordinates enter 2\n");
+        printf("To exit and print the paths enter 3 \n");
         // Evaluationg the option
         static int option;
         scanf("%d", &option);
         printf("\n");
 
-        
-        // Option 1 (Register an account)
+        // choose nodes by ID
         if (option == 1)
         {
             option = 0;
             while ((getchar()) != '\n');
-            register_user();
-
             
+            // Find nodes by ID
+            long long node_id_1;
+            int art_id_1;
+            long long node_id_2;
+            int art_id_2;
+
+            printf("Please enter the id of the node 1: ");
+            scanf("%lli", &node_id_1);
+            if (node_by_ID(node_id_1) == -1)
+            {
+                printf("Sorry the node with this id does not exist!\n\n");
+                continue;
+            }
+            // inpute node 2
+            printf("Please enter the id of the node 2: ");
+            scanf("%lli", &node_id_2);
+            if (node_by_ID(node_id_2) == -1)
+            {
+                printf("Sorry the node with this id does not exist!\n");
+                continue;
+            }
+            // check if they are the same node
+            if (node_by_ID(node_id_2) == node_by_ID(node_id_1))
+            {
+                printf("You have chosen the same nodes!\n");
+                continue;
+            }
+            art_id_1 = node_by_ID(node_id_1);
+            art_id_2 = node_by_ID(node_id_2);
+
+            while (1)
+            {
+                printf("In order to use Dijkstra enter 1, for Floyd enter 2:\n");
+
+                struct NodeArray final_path;
+                // Dijkstra
+                scanf("%d", &option);
+                printf("\n");
+                if (option == 1)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+
+                    // Run Dijkstra's Algorithm
+                    final_path = find_shortest_path_Djikstra (art_id_1, art_id_2);
+                }
+
+                // Floyd
+                else if (option ==2)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+
+                    // Run Floyd's Algorithm
+                    final_path = find_shortest_path_Floyd (art_id_1, art_id_2);
+                }
+                else if (option == 0)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+                    continue;
+                }
+                
+                // Invalid option
+                else
+                {
+                    printf("Sorry, the option you entered was invalid, please try again.\n");
+                    printf("\n");
+                    while ((getchar()) != '\n'); 
+                }
+
+                if (final_path.length > 0)
+                {
+                    show_results(final_path);
+                    break;
+                }
+                
+            }
         }
 
-        //option 2 (Login)
+        // By coordinates
         else if (option ==2)
         {
             option = 0;
             while ((getchar()) != '\n');
 
-            login_user();
+            // Find nodes by coordinates
+            int node_id_1;
+            int node_id_2;
+            double latitude;
+            double longitude;
+
+            // inputting from the user node 1
+            printf("Please enter the latitude of the node 1: ");
+            scanf("%lf", &latitude);
+            printf("Please enter the longitude of the node 1: ");
+            scanf("%lf", &longitude);
+            node_id_1 = find_closest_point(latitude, longitude);
+            if (node_id_1 == -1)
+            {
+                printf("Sorry the node with this coordinates does not exist!\n");
+                continue;
+            }
+            printf("\n");
+            // node 2
+            printf("Please enter the latitude of the node 2: ");
+            scanf("%lf", &latitude);
+            printf("Please enter the longitude of the node 2: ");
+            scanf("%lf", &longitude);
+            node_id_2 = find_closest_point(latitude, longitude);
+            if (node_id_2 == -1)
+            {
+                printf("Sorry the node with this coordinates does not exist!\n");
+                continue;
+            }
+            // chack for the same nodes
+            if (node_id_1 == node_id_2)
+            {
+                printf("You have chosen the same nodes!\n");
+                continue;
+            }
+
+
+            while (1)
+            {
+                printf("In order to use Dijkstra enter 1, for Floyd enter 2:\n");
+
+                struct NodeArray final_path;
+                // Dijkstra
+                scanf("%d", &option);
+                printf("\n");
+                if (option == 1)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+                    // Run Dijkstra
+                    final_path = find_shortest_path_Djikstra (node_id_1, node_id_2);
+                }
+
+                // Floyd
+                else if (option ==2)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+                    // Run Floyd
+                    final_path = find_shortest_path_Floyd (node_id_1, node_id_2);
+                }
+                else if (option == 0)
+                {
+                    option = 0;
+                    while ((getchar()) != '\n');
+                    continue;
+                }
+                
+                // Invalid option
+                else
+                {
+                    printf("Sorry, the option you entered was invalid, please try again.\n");
+                    printf("\n");
+                    while ((getchar()) != '\n'); 
+                }
+
+                if (final_path.length > 0)
+                {
+                    show_results(final_path);
+                    break;
+                }
+                
+            }
             
         }
 
-        //option 3 (Search for books)
+        // EXIT
         else if (option ==3)
         {
             option = 0;
             while ((getchar()) != '\n');
-            find_book();
-        }
-
-        //option 4 (Display all books)
-        else if (option ==4)
-        {
-            option = 0;
-            while ((getchar()) != '\n');
-            // Displaying all books
-            printf("Displaying all books...\n\n");
-            display_book_array(all_books);
-        }
-
-        //option 5 (Quit)
-        else if (option ==5)
-        {
-            option = 0;
-            while ((getchar()) != '\n');
-            printf("Goodbye!\n\n");
-
-            //saving
-            FILE* file;
-            store_books(file);
-            store_users(file);
-            store_loans(file);
-
 
             exit(1);
         }
 
-        //invalid inut
+        else if (option ==0)
+        {
+            option = 0;
+            while ((getchar()) != '\n');
+        }
+
+        //invalid input
         else
         {
-
             printf("Sorry, the option you entered was invalid, please try again.\n");
             printf("\n");
             while ((getchar()) != '\n'); 
         }
-        
-
     }
+    
     return 0;
 }
